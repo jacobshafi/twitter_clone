a247b159defd36ea38ddd6702ed6f1595e8a3a13
Lorraine Chan <lorrainechan@Lorraines-MacBook-Pro.local>
2015-07-30 15:26:18 -0700

    Implement user and session handling

--- db/migrate/20150730140606_create_users.rb
+++ db/migrate/20150730140606_create_users.rb
@@ -3,6 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_hash
+      t.string :email
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20150730140618_create_tweet.rb
+++
@@ -1,4 +0,0 @@
-class CreateTweet < ActiveRecord::Migration
-  def change
-  end
-end
